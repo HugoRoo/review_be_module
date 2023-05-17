c643793037444c81a8222fc25e4609722332d677
hugoRo <ndhugohro@gmail.com>
2023-05-17 17:29:27 -0600

    adding example

--- our-first-scripts/hello-world.js
+++ our-first-scripts/hello-world.js
@@ -3,3 +3,28 @@
 
 
 console.log ('review hola desde Node.js')
+
+
+//DOM
+//alert
+//promp
+
+let myName = "Hugo"
+const HOST = "http://localhost:8080"
+
+console.log(myName);
+console.log(HOST);
+
+//funciones
+
+function suma (a, b) {
+    console.log('suma: ', a+b)
+    return a + b
+}
+
+
+const resta = (a,b) => {
+    console.log('resta: ', a -b )
+    return a - b
+}
+//insert test
